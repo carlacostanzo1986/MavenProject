0e64742ac17124a2794beaba331d724f31b7a28d
Carla Costanzo <carlacostanzo1986@hotmail.com>
2022-03-27 15:53:08 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
 
     public static void main(String[] args) {
-
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
+
     }
 }
